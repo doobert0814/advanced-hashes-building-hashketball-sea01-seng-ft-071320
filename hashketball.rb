2ecd1c38e51ed04700eb1e582826717ccf259d2a
Scot Sexton <rss3509@gmail.com>
2020-06-23 17:42:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,10 +26,3 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
-  game_hash.each do |home_away, keys|
-    keys[:players].each do |player|
-      return player[:points] if player[:player_name] == player_n
-    end
-  end
-end
